4ad69f5be769ab15452cf0adac0ba3a5cb0f9278
mazenais <mazen.aboismail@gmail.com>
2021-11-09 03:36:54 +0100

    creating a UserItem

--- client/src/components/screens/UserItem.tsx
+++ client/src/components/screens/UserItem.tsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import './screens.css';
 
-interface IState {
+interface DataProps {
     item: {
-        id: number,
-        name: string, 
-        age: number,
-        url:string
-
-    } []
+        _id: number;
+        name: string;
+        age: number;
+        url: string;
+    };
+    user: {
+        _id: number;
+        name: string;
+        age: number;
+        url: string;
+    };
 }
 
 const UserItem = (props: any) => {
+    console.log(props.item);
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <Link to={`/users/${props.item._id}`}>
+           
+            <div className="card">
+                <div className="card-inner">
+                    <div className="card-front">
+                    <h3>
+                {props.item.name} 
+            </h3>
+                        <img src={props.item.image} alt="" />
+                    </div>
+                    <div className="card-back">
+                        <h1>{props.item.sex}</h1>
+                        
+                        <ul>
+                            <li>
+                                <strong>Name:</strong> {props.item.name}
+                            </li>
+                            <li>
+                            <strong>Age:</strong> {props.item.age}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+};
 
-export default UserItem
+export default UserItem;
